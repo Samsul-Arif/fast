a06093df5bd8ccdae1eeda072c5bf6499a063f50
Samsul-Arif <samsulsusilo145@gmail.com>
2024-11-26 22:41:32 +0700

    Internet

---
+++ Fast-Internet.sh
@@ -0,0 +1,89 @@
+#!/system/xbin/bash
+clear
+blue='\e[1;34m'
+green='\e[1;32m'                                        
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'                                           
+yellow='\e[1;33m'
+sleep 1
+xdg-open https://youtube.com/@Samz99031
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*="
+toilet -f standard -F gay "   [Fast-Inter]"
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*="
+echo ""              
+echo " Author  : "Samsul Arif"
+" Youtube   : "ARF.YT"
+echo " Kontak  : 085865026059"
+sleep 1
+echo " GitHub  : https://github.com/Samsul-Arif"
+echo " Please For Subcribe Youtube"
+echo "\033[33;1m<{============================}>"
+echo ""
+sleep 1
+echo "\033[31;1m  Silahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[35;1m [===============================================}>"
+sleep 1
+echo "\033[33;1m[1].> \033[36;1mMempercepat / Menstabilkan Jaringan/sinyal"
+echo "\033[35;1m [===============================================}>"
+sleep 1
+echo "\033[33;1m[2].> \033[36;1mMempercepat / menstabilkan jaringan 2"
+echo "\033[35;1m [===============================================}>"
+sleep 1
+echo "\033[33;1m[0].> \033[36;1mkeluar"
+echo "\033[35;1m [===============================================}>"
+sleep 1
+echo "\033[32;1mPilih Nomer Berapa.?==>"
+echo " [===============================================}>"
+read bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "   [Fast-Inter]" | lolcat
+sleep 1
+echo "\033[32;m===========<{Mempercepat / menstabilkan sinyal}>==========="
+sleep 1
+echo "\033[32;1m====================<{By Samsul Arif}>====================="
+sleep 1
+echo ""
+echo ""
+ping 8.8.8.8 
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "   [Fast-Inter]" | lolcat
+sleep 1
+echo "\033[32;m==========<{Mempercepat / menstabilkan sinyal 2}>=========="
+sleep 1
+echo "\033[32;1m====================<{By Samsul Arif}>====================="
+sleep 1
+echo ""
+echo ""
+ping -D www.google.com
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo "\033[32;1mWe Are Family DARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo " "We Are Legion"
+sleep 1
+echo " "We Do Not For Give"
+sleep 1
+echo " "We Do Not For Get"
+sleep 1
+echo " "The Indonesian Anonymous Family"
+sleep 1
+echo " "Expect Us"
+sleep 1
+echo "\033[32;1m Incarlah suatu hal mustahil:) Suatu saat Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
+
